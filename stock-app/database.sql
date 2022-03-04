14976fc409b2baeeba70548fbd6c436881aa2dd3
Brian Leung <hyleung111one@yahoo.com>
2022-03-04 09:55:21 +0800

    all functions working2

--- stock-app/database.sql
+++ stock-app/database.sql
@@ -8,6 +8,7 @@ email varchar(50) not null,
 primary key(username));
 
 create table portfolio(
+entry_num int not null auto_increment,
 username char(20) not null,
 ticker char(10) not null,
 price float,
